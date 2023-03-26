e79317f338379f0aa977f919a5001813f91a6d68
Alexandr Efimchik <efimchiksasha@gmail.com>
2023-03-26 13:46:32 +0300

    refactor: remove unused methods, rename save method to setSides fix: make test's triangle object final

--- src/main/java/grvt/cloud/epam/models/Triangle.java
+++ src/main/java/grvt/cloud/epam/models/Triangle.java
@@ -12,41 +12,18 @@ public class Triangle {
     private Integer b;
     private Integer c;
 
-    public Integer getA() {
-        return a;
-    }
-
-    public void setA(Integer a) {
-        this.a = a;
-    }
-
-    public Integer getB() {
-        return b;
-    }
-
-    public void setB(Integer b) {
-        this.b = b;
-    }
-
-    public Integer getC() {
-        return c;
-    }
-
-    public void setC(Integer c) {
-        this.c = c;
-    }
-
     public Triangle(List<Integer> sides) {
         this.a = sides.get(0);
         this.b = sides.get(1);
         this.c = sides.get(2);
     }
 
-    public void save(Integer a, Integer b, Integer c) {
+    public void setSides(Integer a, Integer b, Integer c) {
         this.a = a;
         this.b = b;
         this.c = c;
     }
+
     public static boolean validateSide(int side) {
         return side > 0;
     }
--- src/test/java/grvt/cloud/epam/controllers/TriangleControllerTest.java
+++ src/test/java/grvt/cloud/epam/controllers/TriangleControllerTest.java
@@ -8,48 +8,48 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 
 class TriangleControllerTest {
-    static Triangle triangle = new Triangle(Arrays.asList(0,0,0));
+     static final Triangle triangle = new Triangle(Arrays.asList(0, 0, 0));
     @Test
     void validateTriangle_exception() {
         Assertions.assertThrows(IllegalArgumentsException.class, () -> {
-            triangle.save(-1,3,1);
+            triangle.setSides(-1, 3, 1);
             triangle.validateTriangle();
         });
     }
 
     @Test
     void checkEquilateral() {
-        triangle.save(1,1,1);
+        triangle.setSides(1, 1, 1);
         Assertions.assertTrue(triangle.checkEquilateral());
 
-        triangle.save(1,2,1);
+        triangle.setSides(1, 2, 1);
         Assertions.assertFalse(triangle.checkEquilateral());
     }
 
     @Test
     void checkIsosceles() {
-        triangle.save(1,2,2);
+        triangle.setSides(1, 2, 2);
         Assertions.assertTrue(triangle.checkIsosceles());
 
-        triangle.save(2,2,1);
+        triangle.setSides(2, 2, 1);
         Assertions.assertTrue(triangle.checkIsosceles());
 
-        triangle.save(2,1,2);
+        triangle.setSides(2, 1, 2);
         Assertions.assertTrue(triangle.checkIsosceles());
 
-        triangle.save(1,2,3);
+        triangle.setSides(1, 2, 3);
         Assertions.assertFalse(triangle.checkIsosceles());
     }
 
     @Test
     void checkRectangular() {
-        triangle.save(3,4,5);
+        triangle.setSides(3, 4, 5);
         Assertions.assertTrue(triangle.checkRectangular());
 
-        triangle.save(6,8,10);
+        triangle.setSides(6, 8, 10);
         Assertions.assertTrue(triangle.checkRectangular());
 
-        triangle.save(1,28,10);
+        triangle.setSides(1, 28, 10);
         Assertions.assertFalse(triangle.checkRectangular());
     }
 }

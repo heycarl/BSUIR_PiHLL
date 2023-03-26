f27677e72985d94d7bf50bb2976cbeddedb04e8f
Alexandr Efimchik <efimchiksasha@gmail.com>
2023-03-27 02:02:17 +0300

    refactor: split model and service

--- src/main/java/grvt/cloud/epam/models/Triangle.java
+++ src/main/java/grvt/cloud/epam/models/Triangle.java
@@ -1,13 +1,32 @@
 package grvt.cloud.epam.models;
 
-import grvt.cloud.epam.exceptions.IllegalArgumentsException;
-
 import java.util.List;
-import java.util.Objects;
-
-import static java.lang.Math.pow;
 
 public class Triangle {
+    public Integer getA() {
+        return a;
+    }
+
+    public void setA(Integer a) {
+        this.a = a;
+    }
+
+    public Integer getB() {
+        return b;
+    }
+
+    public void setB(Integer b) {
+        this.b = b;
+    }
+
+    public Integer getC() {
+        return c;
+    }
+
+    public void setC(Integer c) {
+        this.c = c;
+    }
+
     private Integer a;
     private Integer b;
     private Integer c;
@@ -17,41 +36,4 @@ public class Triangle {
         this.b = sides.get(1);
         this.c = sides.get(2);
     }
-
-    public void setSides(Integer a, Integer b, Integer c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-    }
-
-    public static boolean validateSide(int side) {
-        return side > 0;
-    }
-    public void validateTriangle() throws IllegalArgumentsException {
-        if (!validateSide(a) || !validateSide(b) || !validateSide(c))
-            throw new IllegalArgumentsException();
-    }
-
-    public boolean checkEquilateral() {
-        return Objects.equals(a, b) && Objects.equals(b, c);
-    }
-
-    public boolean checkIsosceles() {
-        return Objects.equals(a, b) || Objects.equals(b, c) || Objects.equals(a, c);
-    }
-
-    @FunctionalInterface
-    interface MathOperation {
-        double calc(double a);
-    }
-
-    public boolean checkRectangular() {
-        MathOperation sq;
-        sq = (x) -> pow(x, 2);
-
-        return sq.calc(a) == sq.calc(b) + sq.calc(c) ||
-                sq.calc(b) == sq.calc(a) + sq.calc(c) ||
-                sq.calc(c) == sq.calc(a) + sq.calc(b);
-    }
-
 }
---
+++ src/main/java/grvt/cloud/epam/services/TriangleService.java
@@ -0,0 +1,58 @@
+package grvt.cloud.epam.services;
+
+import grvt.cloud.epam.exceptions.IllegalArgumentsException;
+import grvt.cloud.epam.models.Triangle;
+
+import java.util.List;
+import java.util.Objects;
+
+import static java.lang.Math.pow;
+
+public class TriangleService {
+    private final Triangle triangle;
+
+    public TriangleService(List<Integer> sides) {
+        this.triangle = new Triangle(sides);
+    }
+
+    public void setSides(Integer a, Integer b, Integer c) {
+        triangle.setA(a);
+        triangle.setB(b);
+        triangle.setC(c);
+    }
+
+    public boolean checkEquilateral() {
+        return Objects.equals(triangle.getA(), triangle.getB())
+                && Objects.equals(triangle.getB(), triangle.getC());
+    }
+
+    public boolean checkIsosceles() {
+        return Objects.equals(triangle.getA(), triangle.getB()) ||
+                Objects.equals(triangle.getB(), triangle.getC()) ||
+                Objects.equals(triangle.getA(), triangle.getC());
+    }
+
+    @FunctionalInterface
+    interface MathOperation {
+        double calc(double a);
+    }
+
+    public boolean checkRectangular() {
+        MathOperation sq;
+        sq = (x) -> pow(x, 2);
+
+        return sq.calc(triangle.getA()) == sq.calc(triangle.getB()) + sq.calc(triangle.getC()) ||
+                sq.calc(triangle.getB()) == sq.calc(triangle.getA()) + sq.calc(triangle.getC()) ||
+                sq.calc(triangle.getC()) == sq.calc(triangle.getA()) + sq.calc(triangle.getB());
+    }
+
+    public static boolean checkInvalidSide(int side) {
+        return side <= 0;
+    }
+
+    public void validateTriangle() throws IllegalArgumentsException {
+        if (checkInvalidSide(triangle.getA()) || checkInvalidSide(triangle.getB()) || checkInvalidSide(triangle.getC()))
+            throw new IllegalArgumentsException();
+    }
+
+}
